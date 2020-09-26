e476107b180824bd8fd4bf208f83b931788189a9
Lauren De Los Santos <lauren.301371@nv.ccsd.net>
2020-09-26 03:12:27 +0000

    I added comments in HowFarAway

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -7,30 +7,40 @@ public class HowFarAway
         double startLat; //starting latitude
         double startLong; //starting longitude
         double endLat; //ending latitude
-        double endLong; //ending latitude
-        double distance; //distance between two points
+        double endLong; //ending longitude
+        double distance; //distance between the two points
 
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
 
+        //Asks the user to enter the latitude of the starting location
         System.out.print("Enter the latitude of the starting location: ");
         startLat = scanner.nextDouble();
 
+        //Asks the user to enter the longitude of the starting location
         System.out.print("Enter the longitude of the starting location: ");
         startLong = scanner.nextDouble();
 
+        //Asks the user to enter the latitude of the ending location
         System.out.print("Enter the latitude of the ending location: ");
         endLat = scanner.nextDouble();
 
+        //Asks the user to enter the longitude of the ending location
         System.out.print("Enter the longitude of the ending location: ");
         endLong = scanner.nextDouble();
 
+        //Creates a new GeoLocation object for the starting location
+        //and initializes its variables
         GeoLocation myStartGeoLocation = new GeoLocation(startLat, startLong);
 
+        //Creates a new GeoLocation object for the ending location
+        //and initializes its variables
         GeoLocation myEndGeoLocation = new GeoLocation(endLat, endLong);
 
+        //Gets the distance between the starting and ending locations
         distance = myStartGeoLocation.distanceFrom(myEndGeoLocation);
 
+        //Prints out the distance
         System.out.print("The distance is " + distance + " miles.");
     }
 }
